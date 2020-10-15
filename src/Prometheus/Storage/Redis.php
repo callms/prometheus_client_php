51cf22960f64814f4e372f0a1552a98c50ed42aa
Christopher Allène <christopher.allene@mobsuccess.com>
2020-10-15 20:12:15 +0200

    Fix detection of dead Redis servers

--- src/Prometheus/Storage/Redis.php
+++ src/Prometheus/Storage/Redis.php
@@ -129,7 +129,11 @@ class Redis implements Adapter
                 @$this->redis->pconnect($this->options['host'], $this->options['port'], $this->options['timeout']);
                 for($i = 0; $i < static::MAX_RETRY_OPEN_CONNECTION; $i++) {
                     try {
-                        $isOk = $this->redis->ping('+PONG') === '+PONG';
+                        if('2.2.5' === phpversion('redis')) {
+                            $isOk = $this->redis->ping() === '+PONG';
+                        } else {
+                            $isOk = $this->redis->ping('+PONG') === '+PONG';
+                        }
                     } catch(\Exception $e) {
                     }
                     if($isOk) {
